3793d27cdab4e331ad5276e8e439b1c08da4d03a
Erick Garcia <eggarcia9814@gmail.com>
2021-04-26 12:07:26 -0500

    React Basic

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,14 @@
+import TodoItem from './TodoItem'
+
+export default function TodoList({todos, toggleTodo}) {
+
+    return (
+        
+        <ul>
+            {todos.map(todo => (
+                <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo}/>
+            ))}
+            
+        </ul>)
+  ;
+}
